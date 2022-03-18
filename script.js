e6a3c70c75dd9b7a6ea4d709a008e0b6690f624b
Morgan Kooman <92802713+morgankooman@users.noreply.github.com>
2022-03-18 11:16:27 +0100

    Commit 4 I think
    I have made new windows open when links are clicked and fixed the dark/light button toggle

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-function darkMode() {
+const darkMode = () => {
     let element = document.body;
-    element.classList.toggle("dark");
+    element.classList.toggle("light");
+
+    const mode = document.getElementById("dark-light");
+
+    mode.innerHTML == "Light mode" ? mode.innerHTML = "Dark mode" : mode.innerHTML = "Light mode";
+
     }
+
+
+
+
